35b75709df48a20f6d711a93feda0fcaf969ef0e
tkp153 <takap365best@gmail.com>
2023-03-16 16:34:04 +0900

    GUI出力できないバグ修正

--- deeplearning_cuda/docker_run.bash
+++ deeplearning_cuda/docker_run.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 xhost +
-docker run --restart always --shm-size=8000m  -it --gpus all --privileged --network host --name deeplearning_book --group-add video --env DISPLAY --volume /tmp/.X11-unix:/tmp/.X11-unix:rw --volume /dev:/dev:rw --volume ${PWD}:/root/WORKDIR:rw  deeplearning_cuda:1.0
+docker run --restart always --shm-size=8000m  -it --gpus all --privileged --network host --name deeplearning_book --group-add video --env DISPLAY="$DISPLAY" --volume /tmp/.X11-unix:/tmp/.X11-unix:rw --volume /dev:/dev:rw --volume ${PWD}:/root/WORKDIR:rw  deeplearning_cuda:1.0
