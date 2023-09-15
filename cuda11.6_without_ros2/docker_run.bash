1dda12456f50ebc0871917fa0d6156ea295b8596
takkun-jpg <e1x20018@oit.ac.jp>
2023-09-15 21:08:34 +0900

    Realsense　Ubuntu22.04とROS２対応　Image作成

--- cuda11.6_without_ros2/docker_run.bash
+++ cuda11.6_without_ros2/docker_run.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 xhost +
-docker run --restart always --shm-size=8000m  -it --gpus all --privileged --network host --name gala_workspaces --group-add video --env DISPLAY --volume /tmp/.X11-unix:/tmp/.X11-unix:rw --volume /dev:/dev:rw --volume ${PWD}:/root/WORKDIR:rw  tkp153/reserachv1:latest
+docker run --restart always --shm-size=8000m  -it --gpus all --privileged --network host --name test --group-add video --env DISPLAY --volume /tmp/.X11-unix:/tmp/.X11-unix:rw --volume /dev:/dev:rw --volume ${PWD}:/root/WORKDIR:rw ubuntu:jammy-20230624
+
